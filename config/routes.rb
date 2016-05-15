4569f627dc9731fc3e8fc0265fa74e4b1d34a80b
Enzo Zerega <eczerega@MacBook-Pro-de-Enzo-2.local>
2016-05-15 09:11:34 -0300

    ola vvasd

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'home#index'
 
+  post 'instagram/tag/buscar' => 'home#buscar', defaults: {format: :json}
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
